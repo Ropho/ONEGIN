b1f7675b7aef8e3bbc31682cf18d456d952f7da0
Ropho <89077943+Ropho@users.noreply.github.com>
2021-08-25 14:26:04 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,33 +6,36 @@ const int line   = 3;
 
 int main (void) {
 
-    char array_strings [line][column];
+    char array_strings [line][column] ={};
 
     for (int i = 0; i < line; ++i) {          //vvod ((((
 
         gets (array_strings[i]);
         //puts (array_strings[i]);
     }
-
- /*for (int i = 0; i < column; ++i) {        // ne robit
+/*
+ for (int i = 0; i < line; ++i) {        // ne robit
 
         puts (array_strings[i]);
     }
 */
-    for(int i = 1; i < line; i++) {
-            char str[100] = "";
+    for(int i = 1; i < line; ++i) {                 //пузырек
+            char str[column] = "";
 
-        for(int j = 0; j < column - i; j++)
-            if(strcmp(array_strings[j], array_strings[j+1]) > 0){
-                strcpy(str, array_strings[j]);
-                strcpy(array_strings[j], array_strings[j+1]);
-                strcpy(array_strings[j+1], str);
+        for(int j = 0; j < line - i; ++j) {
+
+            if (strcmp(array_strings[j], array_strings[j+1]) > 0){
+                    strcpy(str, array_strings[j]);
+                    strcpy(array_strings[j], array_strings[j+1]);
+                    strcpy(array_strings[j+1], str);
+                    //puts (str);
             }
+            //puts (str);
     }
+}
+//return 0;
+ for (int i = 0; i < line; ++i) {        // ne robit
 
-    for (int i = 0; i < line; ++i) {          //vivod posimvolno
-        for (int j = 0; j < column; ++j)
-            putchar (array_strings[i][j]);
-        printf("\n");
+        puts (array_strings[i]);
     }
 }
