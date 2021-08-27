5edeb0441110abdb4d398e23cbb8955e96b0c80c
Ropho <89077943+Ropho@users.noreply.github.com>
2021-08-27 14:56:11 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include "string.h"
 #include "assert.h"
 #include "malloc.h"
+#include "sys\stat.h"
 
 const int column = 100;
 
 typedef struct JOJO {
     int len_str;
-    char str[column];
+    char *str;
 }JOJO;
 
 
@@ -32,19 +33,36 @@ int FILESIZE_FUNC_VLOB (FILE *in) {
 	return file_size;
 }
 
-/*
-int FILESIZE_FUNC (FILE *fp) {
-    assert (fp != NULL);
-    int save_pos = 0, size_of_file = 0;
-    save_pos = ftell (fp);
-    fseek (fp, 0L, SEEK_END);
-    size_of_file = ftell (fp);
-    fseek (fp, save_pos, SEEK_SET);
+
+int FILESIZE_FUNC (FILE *in) {
+
+    assert (in != NULL);
+
+    int size_of_file = 0;
+
+    fseek (in, 0, SEEK_END);
+
+    size_of_file = ftell (in);
+
+    fseek (in, 0, SEEK_SET);
+
     return (size_of_file);
+}
+/*
+int FILESIZE_FUNC_FSTAT (FILE *in) {
+
+    assert (in != NULL);
+
+    struct stat buff;
+
+    fstat (fileno (in), &buff);
+
+    return buff.st_size;
 }
 */
 
-int number_lines_in_array (char* str, int filesize) {
+/*
+int number_lines_in_array_VLOB (char* str, int filesize) {
 
     assert (str != NULL);
 
@@ -61,6 +79,25 @@ int number_lines_in_array (char* str, int filesize) {
     num_of_lines++;
 
     return num_of_lines;
+}
+*/
+
+int number_lines_in_array_LEHA (char* str) {
+
+    assert (str != NULL);
+
+    int num_of_lines = 0;
+
+
+    char *starto = str;
+
+    while (strchr (starto, '\n') != NULL) {
+
+        ++num_of_lines;
+        starto = strchr (starto, '\n') + 1;
+    }
+
+    return num_of_lines + 1;
 }
 
 
@@ -88,7 +125,7 @@ void JOJO_FILLIN (char* str, JOJO* array_sort, int number_lines) {
 
         if (i == number_lines - 1) {
 
-        strcpy (array_sort[i].str, starto);
+        array_sort[i].str = starto;
         array_sort[i].len_str = strlen (array_sort[i].str);
         //printf ("%s %d\n", array_sort[i].str, array_sort[i].len_str);
         return;
@@ -98,7 +135,7 @@ void JOJO_FILLIN (char* str, JOJO* array_sort, int number_lines) {
         char *endo = strchr (starto, '\n');
         *endo = '\0';
 
-        strcpy (array_sort[i].str, starto);
+        array_sort[i].str = starto;
         array_sort[i].len_str = endo - starto;
 
         //printf ("%s %d\n", array_sort[i].str, array_sort[i].len_str);
@@ -112,20 +149,20 @@ void sort_array_BUBBLE (JOJO *str, int lines) {
 
     assert (str != NULL);
 
-     for (int i = 1; i < lines; ++i) {                 //BUBBLE SORT
+     for (int i = 1; i < lines; ++i) {
             JOJO s = {};
 
         for (int j = 0; j < lines - i; ++j) {
 
-            if (strcmp(str[j].str, str[j+1].str) > 0){
+            if (strcmp(str[j].str, str[j+1].str) > 0) {
 
-                    strcpy(s.str, str[j].str);
+                    s.str = str[j].str;
                     s.len_str = str[j].len_str;
 
-                    strcpy(str[j].str, str[j+1].str);
+                    str[j].str = str[j+1].str;
                     str[j].len_str = str[j+1].len_str;
 
-                    strcpy(str[j+1].str, s.str);
+                    str[j+1].str = s.str;
                     str[j+1].len_str = s.len_str;
 
             }
@@ -172,7 +209,10 @@ int main (void) {
 
     FILE *in = fopen ("HAMLET.txt", "r");
     FILE *out = fopen ("HAMLET_SORTED.txt", "w");
-    if (in == NULL) {
+    FILE *in_binary = fopen ("HAMLET.txt", "rb");
+
+    if (in == NULL || out == NULL || in_binary == NULL) {
+
         puts ("FILE_NOT_FOUND");
 
         return ERROR_FILE_INCORRECT;
@@ -181,8 +221,19 @@ int main (void) {
 
     int filesize = FILESIZE_FUNC_VLOB (in);
 
+    //int filesize_LEHA = FILESIZE_FUNC_FSTAT (in_binary);
+
+   // printf ("%d\n%d", filesize, filesize_LEHA);
+   // return 0;
+   /////
+
+
+
+
     char *array_strings = (char*)malloc(filesize * sizeof(char) + 1);
+
     if (array_strings == NULL) {
+
       puts ("ERROR IN MEMORY ALLOCATION");
 
       return ALLOCATION_MEMORY_ERROR;
@@ -191,6 +242,7 @@ int main (void) {
     fseek (in, 0, SEEK_SET);
 
     if (fread (array_strings, 1, filesize, in) != filesize) {
+
       puts ("ERROR IN READIN FROM FILE");
 
       return ERROR_IN_READING_FROM_FILE;
@@ -199,10 +251,8 @@ int main (void) {
     array_strings[filesize] = '\0';
 
 
-    fclose (in);
-
-    int number_lines = number_lines_in_array (array_strings,filesize);
-
+    //int number_lines = number_lines_in_array_VLOB (array_strings,filesize);
+    int number_lines = number_lines_in_array_LEHA (array_strings);
 
     JOJO array_sort[number_lines] = {};
 
@@ -214,16 +264,10 @@ int main (void) {
 
     output_ne_sorted (array_strings, number_lines, out);
 
+
+    fclose (in);
     fclose (out);
-/*
-    copy_arrays (array_nesort, array_sort, lines);
-    bubble_sort (array_sort, lines);
-    FILE *out = fopen ("HAMLET_SORTED.txt", "w");
-        if (out == NULL)
-            return ERROR_FILE_INCORRECT;
-    output_in_file (out, array_sort,   lines);
-    output_in_file (out, array_nesort, lines);
-    fclose (out);
-*/
+    fclose (in_binary);
+
 return 0;
 }
